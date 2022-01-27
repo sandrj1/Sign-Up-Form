eee97f6347a0958fc47e661568f497613285312b
Sandra Bote <sandra.bote98@gmail.com>
2022-01-27 19:31:53 +0100

    data validation + media queries

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const input = [fname, lname, email, password];
 
 function validateForm() {
     let counter = 0;
+    let validEmail = false;
 
     for (let i = 0; i < input.length; i++) {
         let inputValue = input[i].value.trim();
@@ -25,40 +26,96 @@ function validateForm() {
             message.style.display = "none";
         }
 
-        if (inputValue.length === 0) {
-            emptyField();
+        function focusEmptyField() {
+            box.style.outline = "1px solid var(--blue)";
+        }
 
-            // Highlight empty field when enter is being pressed.
+        function focusInvalidField() {
             box.style.outline = "transparent";
-            function focusEmptyField() {
-                box.style.outline = "1px solid var(--blue)";
-            }
             input[i].addEventListener("focus", focusEmptyField, true);
             input[i].addEventListener("keyup", focusEmptyField, true);
+        }
 
-            // Hover all fields.
-            input[i].addEventListener("mouseenter", focusEmptyField, true);
-            input[i].addEventListener("mouseleave", function () {
-                box.style.outline = "transparent";
-            });
+        function focusNextField() {
+            box.style.outline = "transparent";
+            input[i + 1].focus();
+            let nextBox = input[i + 1].parentNode;
+            nextBox.style.outline = "1px solid var(--blue)";
+        }
+
+        // Hover all fields.
+        input[i].addEventListener("mouseenter", focusEmptyField, true);
+        input[i].addEventListener("mouseleave", function () {
+            box.style.outline = "transparent";
+        });
+
+        // Remove/add error when typing.
+        input[i].addEventListener("keyup", function (e) {
+            let value = e.target.value.trim()
+            if (value.length > 0) {
+                correctField();
+            } else if (e.keyCode === 8 && value.length === 0) {
+                if (input[i] === email) {
+                    message.innerHTML = "Email Address cannot be empty";
+                }
+                emptyField();
+            }
+        })
+
+        // Show/remove error.
+        if (inputValue.length === 0) {
+            emptyField();
+            focusInvalidField();
 
         } else if (inputValue.length > 0) {
-            correctField();
-            counter = counter + 1;
+
+            if (input[i] === email) {
+                let at = inputValue.indexOf("@");
+                let dot = inputValue.lastIndexOf(".");
+
+                if (!(inputValue.includes("@") &&
+                    inputValue.includes(".") &&
+                    !inputValue.startsWith("@") &&
+                    !inputValue.endsWith("@") &&
+                    !inputValue.startsWith(".") &&
+                    !inputValue.endsWith(".") &&
+                    (dot - at > 2))
+                ) {
+                    message.innerHTML = "Looks like this is not an email";
+                    emptyField();
+                    focusInvalidField();
+
+                } else {
+                    validEmail = true;
+                    correctField();
+                }
+
+            } else {
+                correctField();
+            }
 
             // Highlight next empty field.
             input[i].addEventListener("keyup", function (e) {
                 if (e.keyCode === 13) {
-                    box.style.outline = "transparent";
-                    input[i + 1].focus();
-                    let nextBox = input[i + 1].parentNode;
-                    nextBox.style.outline = "1px solid var(--blue)";
+                    if (input[i + 1] !== password) {
+                        focusNextField();
+
+                    } else if (input[i] === email) {
+                        if (!validEmail) {
+                            emptyField();
+                        } else {
+                            focusNextField();
+                        }
+                    }
                 }
             })
+
+            counter = counter + 1;
         }
     }
-    
-    if (counter === input.length) {
+
+    // Submits only when all fields are correctly filled in.
+    if (counter === input.length && validEmail === true) {
         return true;
     } else {
         return false;
